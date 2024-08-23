f8122c444f989382ab99d6607b7de4d015fa0732
jayantprasaddd <jayantpd21@gmail.com>
2024-08-23 22:56:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+function revealSurprise() {
+    const surprise = document.getElementById('surprise');
+    surprise.style.display = 'block';
+    surprise.style.animation = 'fadeIn 2s ease-in-out';
+}
